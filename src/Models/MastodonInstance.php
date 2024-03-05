cef58e740e80a2120a9417c0e80d7ab9b9cf1ffd
Nic Barr <1134853+theothernic@users.noreply.github.com>
2024-03-04 22:15:09 -0600

    Get the data flowing properly into the model

--- src/Models/MastodonInstance.php
+++ src/Models/MastodonInstance.php
@@ -6,23 +6,22 @@
     class MastodonInstance extends DataModel
     {
 
-        public string $id;
+        public ?string $id;
 
-        public string $name;
+        public ?string $name;
 
-        public string $registrations;
+        public ?string $registrations;
 
-        public string $version;
+        public ?string $version;
 
-        public string $ipv6;
+        public ?string $ipv6;
 
-        public float $uptime;
+        public float $uptime = 0.0;
 
-        public string $https_score;
-        public string $obs_score;
+        public ?string $https_score;
+        public ?string $obs_score;
         public int $users = 0;
         public int $statuses = 0;
-
         public int $connections = 0;
 
     }
